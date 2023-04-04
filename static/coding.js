81fb4fce476c30b0ff484281ca22b8d1d8229451
chiubowen <cbh@cameo.tw>
2023-04-04 23:07:09 +0800

    <button type="submit" class="btn btn-primary" id="submit-btn">寫吧</button>         <button class="btn btn-primary" id="run-btn">跑吧</button>

--- static/coding.js
+++ static/coding.js
@@ -45,3 +45,21 @@ questionForm.addEventListener('submit', async (e) => {
     editor.setValue('An error occurred. Please try again.')
   }
 })
+
+// 新增按鈕點擊事件
+const runBtn = document.getElementById('run-btn')
+runBtn.addEventListener('click', () => {
+  const html = editor.getValue() // 取得編輯器內容
+  const parser = new DOMParser() // 建立 DOMParser
+  const doc = parser.parseFromString(html, 'text/html') // 將 HTML 字串轉換成 DOM 物件
+  const body = doc.querySelector('body') // 取得 body 元素
+
+  // 將 HTML 結果顯示於 modal 中
+  const modal = new bootstrap.Modal(document.getElementById('html-modal'))
+  const iframe = document.getElementById('html-iframe')
+  iframe.src = 'about:blank'
+  iframe.contentWindow.document.open()
+  iframe.contentWindow.document.write(body.innerHTML) // 將 body 元素內容寫入 iframe
+  iframe.contentWindow.document.close()
+  modal.show()
+})
